5f64ce7e5d2602444f9b91da1675b65968aee1cc
Varshinee <vvarshinee@gmail.com>
2023-04-17 22:04:11 -0500

    rest api integration

---
+++ src/index.jsx
@@ -0,0 +1,46 @@
+import api, { route } from "@forge/api";
+import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState, ProjectPage, StatusLozenge } from "@forge/ui";
+
+
+const fetchCommentsForIssue = async (issueIdOrKey) => {
+  const res = await api
+    .asUser()
+    .requestJira(route`/rest/api/3/issue/${issueIdOrKey}/comment`);
+
+  const data = await res.json();
+  return data.comments;
+};
+
+const App = () => {
+  const context = useProductContext();
+  const [comments] = useState(async () => await fetchCommentsForIssue(context.platformContext.issueKey));
+
+  console.log(`Number of comments on this issue: ${comments.length}`);
+
+  return (
+    <Fragment>
+      <Text>Planify Estimate: <StatusLozenge text="2" appearance="inprogress" /> </Text>
+    </Fragment>
+  );
+};
+
+export const run = render(
+  <IssuePanel>
+    <App />
+  </IssuePanel>
+);
+
+const Project = () => {
+  //   const context = useProductContext();
+  //   const [comments] = useState(async () => await fetchCommentsForIssue(context.platformContext.issueKey));
+  //   console.log(`Number of comments on this issue: ${comments.length}`);
+
+  return (
+    <ProjectPage>
+      <Text>Hello, world!</Text>
+    </ProjectPage>
+
+  );
+};
+
+export const projRun = render(<Project />);
---
+++ src/project.jsx
@@ -0,0 +1,59 @@
+import api, { route } from "@forge/api";
+import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState, ProjectPage, StatusLozenge } from "@forge/ui";
+
+
+const fetchEvents = async () => {
+    const res = await api
+        .asUser()
+        .requestJira(route`/rest/api/3/search?jql=project=TCP`);
+    const data = await res.json();
+    return data;
+};
+
+
+const App = () => {
+
+    const events = useState(async () => await fetchEvents());
+    var issueArr = []
+
+    events[0].issues.forEach(issue => {
+        var issueDescriptionMap = {}
+        if (issue.fields.issuetype.name == 'Task') {
+
+            var descArr = []
+            issue.fields.description.content.forEach(contentElement => {
+
+                contentElement.content.forEach(content => {
+                    if (content.type = "Text") {
+                        descArr.push(content.text)
+                    }
+                });
+
+            });
+            issueDescriptionMap.type = issue.fields.summary;
+            issueDescriptionMap.description = descArr
+            issueArr.push(issueDescriptionMap)
+        }
+    });
+
+    // var issueArrStr = JSON.stringify(issueArr)
+
+    return (
+        <ProjectPage>
+            {issueArr.map(function (issue, i) {
+                return <Fragment>
+                    <Text> TASK: <StatusLozenge text={issue.type} appearance="inprogress" /></Text>
+                    {
+                        issue.description.map(function (desc, i) {
+                            return <Text>DESC: {desc}</Text>
+                        })
+                    }
+                    <Text>................................................................</Text>
+                </Fragment>;
+            })
+            }
+        </ProjectPage>
+    );
+};
+
+export const run = render(<App />);
