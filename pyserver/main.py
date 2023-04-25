19669d12c2f4d350dd4b95031178868c2e380858
Varshinee <vvarshinee@gmail.com>
2023-04-25 15:55:44 -0500

    assignee prediction

--- pyserver/main.py
+++ pyserver/main.py
@@ -7,77 +7,180 @@ import pickle
 from tensorflow.keras.models import load_model
 import tensorflow as tf
 from flask import jsonify
-
-
+import pickle 
+import string
 
 app = Flask(__name__)
 
 
-@app.route('/train', methods=['POST'])
-def train():
-    # Auto Keras Regression
+def process_string(input_str):
+    # removing punct
+    input_str = input_str.translate(str.maketrans('', '', string.punctuation))
+    input_str = input_str.lower()
+    return input_str
 
-    from autokeras import TextRegressor
-    text_reg = TextRegressor(overwrite=True, max_trials=1)
-    reqInput = request.json
-    
-    df_train = pd.DataFrame()
+
+def prepare_test_dateaset(features, reqInput ):
     df_test = pd.DataFrame()
-
-    features = ["SUMMARY"]
-    lable = "STORY POINT ESTIMATE"
-    
     for f in features:
         fset = {}
         farr = []
         for r in reqInput:
-            farr.append(r[f])
+            farr.append(process_string(r[f]))
+        fset[f] = farr
+    
+    for f in features:
+        df_test[f] = fset[f]
+
+    df_test_reindex = df_test.reindex()
+    testX = df_test_reindex
+    x_test = np.array(testX)
+    return x_test
+
+
+
+def prepare_train_dataset(features, label, reqInput, feature_defaults):
+    
+    df_train = pd.DataFrame()
+    df_test = pd.DataFrame()
+    for f in features:
+        fset = {}
+        farr = []
+        for r in reqInput:
+            farr.append(process_string(r[f]))
         fset[f] = farr
     
     larr = []
     for r in reqInput:
-        larr.append(r[lable])
+        larr.append(r[label])
     
     for f in features:
         df_train[f] = fset[f]
 
-    df_test['lable'] = larr
+    df_test['label'] = larr
 
-    df_train_reindex = df_train.reindex(range(257), fill_value='')
-    df_test_reindex = df_test.reindex(range(257), fill_value=0)
+    if(len(df_train) < 20):
+        df_train_reindex = df_train.reindex(range(20), fill_value='')
+        df_test_reindex = df_test.reindex(range(20), fill_value=feature_defaults[label])
+    else:
+        df_train_reindex = df_train.reindex()
+        df_test_reindex = df_test.reindex()
+
+    metadata = {
+        "labels" : sorted(df_test_reindex['label'].unique())
+    }
+
+    with open('label_metadata.pkl', 'wb') as f:
+        pickle.dump(metadata, f)
 
 
     trainX=df_train_reindex
     trainY=df_test_reindex
 
-
     x_train = np.array(trainX)
     y_train = np.array(trainY)
 
-    text_reg.fit(x_train, y_train, epochs=50)
+    return x_train, y_train
+
+@app.route('/train', methods=['POST'])
+def train():
+    # Auto Keras Regression
+
+    # Training Story Point estimate
+    from autokeras import TextRegressor
+    import autokeras as ak
+
+    feature_defaults = {
+        "STORY POINT ESTIMATE": 0,
+        "ASSIGNEE": '',
+
+    }
+
+    text_reg = TextRegressor(overwrite=True, max_trials=1)
+    reqInput = request.json
+
+    features = ["SUMMARY"]
+    label = "STORY POINT ESTIMATE"
+
+    x_train, y_train = prepare_train_dataset(features,label,reqInput, feature_defaults  )
+
+    text_reg.fit(x_train, y_train, epochs=70, batch_size=10 )
     model = text_reg.export_model()
     model.save('./models/spe_model.tf')
 
-    return reqInput
+    # Training Assignee estimate
+
+    features = ["SUMMARY"]
+    label = "ASSIGNEE"
+
+    x_train, y_train = prepare_train_dataset(features,label,reqInput,feature_defaults  )
+
+    print(y_train)
     
+    clf = ak.TextClassifier(
+        overwrite=True, max_trials=1
+    )
+
+    clf.fit(x_train, y_train, epochs=70, batch_size=10 )
+    model = clf.export_model()
+    model.save('./models/assignee_model.tf')
+
+    return "Trained Successfully"
+
+@app.route('/predict_assignee', methods=['POST'])
+def predict_assignee():
+    
+    reqInput = request.json
+
+    loaded_model = load_model("./models/assignee_model.tf")
+
+    x_test = prepare_test_dateaset(['SUMMARY'], reqInput)
+
+    predicted_y = loaded_model.predict(tf.expand_dims(x_test, -1))
+
+    predicted_class = []
+
+    with open('label_metadata.pkl', 'rb') as f:
+        label_classes = pickle.load(f)
+
+    for i in predicted_y:
+        predicted_class.append(label_classes["labels"][i.argmax(axis=-1)])
+
+
+    response = {
+        "DATA_PREDICTION": predicted_class
+    }
+
+    print(label_classes)
+    return json.dumps(response)
+
+
 @app.route('/test', methods=['POST'])
 def test():
     # Auto Keras Regression
 
     reqInput = request.json
-    
+
     loaded_model = load_model("./models/spe_model.tf")
 
-    predicted_y = loaded_model.predict(tf.expand_dims(reqInput["SUMMARY"], -1))[0]
-    
-    print(predicted_y.round())
+    x_test = prepare_test_dateaset(['SUMMARY'], reqInput)
+
+    predicted_y = loaded_model.predict(tf.expand_dims(x_test, -1))
+
+    predicted_class = []
+
+    for i in predicted_y:
+        print(i)
+        predicted_class.append(i[0].round().tolist())
+
 
     response = {
-        "PREDICTION_ESTIMATE": predicted_y.round().tolist()
+        "DATA_PREDICTION": predicted_class
     }
 
-    print(response)
-    return json.dumps(response)
+    print(predicted_class)
+
+    return json.dumps(response)   
 
 
 @app.route('/')
