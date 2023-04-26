ad20e6753e2a801b7341627928e695ac94cb9ae9
Varshinee <vvarshinee@gmail.com>
2023-04-25 20:26:35 -0500

    LR commit

--- pyserver/main.py
+++ pyserver/main.py
@@ -9,6 +9,8 @@ import tensorflow as tf
 from flask import jsonify
 import pickle 
 import string
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
 
 app = Flask(__name__)
 
@@ -125,33 +127,72 @@ def train():
     model = clf.export_model()
     model.save('./models/assignee_model.tf')
 
+
+    # Assignee LR Model
+    X = x_train.reshape(x_train.shape[0])
+    y = y_train.reshape(y_train.shape[0])
+    print("X.shape::::")
+    print(X.shape)
+    print(y.shape)
+    cv = CountVectorizer()
+    X_train_cv = cv.fit_transform(X)
+    lr = LogisticRegression()
+    lr.fit(X_train_cv, y)
+    pickle.dump(lr, open("./models/lr.sav", 'wb'))
+    pickle.dump(cv, open("./models/lr_countVect.sav", 'wb'))
     return "Trained Successfully"
 
 @app.route('/predict_assignee', methods=['POST'])
 def predict_assignee():
-    
-    reqInput = request.json
 
-    loaded_model = load_model("./models/assignee_model.tf")
+    model = request.json["model"]
+
+    predicted_class = []
+    
+    reqInput = request.json["data"]
+
+    print(reqInput)
 
     x_test = prepare_test_dateaset(['SUMMARY'], reqInput)
 
-    predicted_y = loaded_model.predict(tf.expand_dims(x_test, -1))
+    if(model == "assignee_model.tf"):
+        loaded_model = load_model("./models/assignee_model.tf")
+        predicted_y = loaded_model.predict(tf.expand_dims(x_test, -1))
+        
 
-    predicted_class = []
+        with open('label_metadata.pkl', 'rb') as f:
+            label_classes = pickle.load(f)
 
-    with open('label_metadata.pkl', 'rb') as f:
-        label_classes = pickle.load(f)
+        for i in predicted_y:
+            predicted_class.append(label_classes["labels"][i.argmax(axis=-1)])
 
-    for i in predicted_y:
-        predicted_class.append(label_classes["labels"][i.argmax(axis=-1)])
+        print(label_classes)
+
+    elif model == "lr.sav":
+        loaded_model = pickle.load(open("./models/lr.sav", 'rb'))
+        loaded_cv_model = pickle.load(open("./models/lr_countVect.sav", 'rb'))
+
+        # Assignee LR Model
+        x = x_test.reshape(x_test.shape[0])
+
+        x_test_cv = loaded_cv_model.transform(x)
+
+        # generate predictions
+        predictions = loaded_model.predict(x_test_cv)
+        predicted_class = predictions.tolist()
+        print(predicted_class)
+        
+        
+        print(predictions)
 
 
     response = {
         "DATA_PREDICTION": predicted_class
     }
 
-    print(label_classes)
+    print(response)
+
+   
     return json.dumps(response)
 
 
@@ -159,7 +200,7 @@ def predict_assignee():
 def predict_spe():
     # Auto Keras Regression
 
-    reqInput = request.json
+    reqInput = request.json["data"]
 
     loaded_model = load_model("./models/spe_model.tf")
 
