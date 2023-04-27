70333a9812afca347658c792e50b1548b598147f
Naveen <naveenkumar.nallasamy@cdw.com>
2023-04-28 03:01:38 +0530

    code refactoring
    Refactored front end code

--- src/index.jsx
+++ src/index.jsx
@@ -1,88 +1,27 @@
 import api, { route, fetch } from "@forge/api";
 
-import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState, ProjectPage, StatusLozenge,MacroConfig ,Table, Head, Cell, Row,Select, Option, Button, Checkbox, CheckboxGroup, Form, useEffect, Heading, Badge, Strong, ButtonSet } from "@forge/ui";
-import { useTable } from 'react-table';
+import ForgeUI, { render, Fragment, Text, useState, ProjectPage, StatusLozenge ,Table, Head, Cell, Row, Button, useEffect, Heading, ButtonSet } from "@forge/ui";
 
-// const [events, setEvents] = useState('');
+// Application Constants
+const appConstants =  {
+    TABLE_HEADER_CONSTANTS : {
+        STORY_NAME : "Story Name",
+        DESCRIPTION : "Description/Summary",
+        STORY_POINTS : "Estimated Story Points",
+        SUGGESTED_DEVELOPER : "Suggested Developer"
+    },
+    PREDICT_STORY_POINT_URL : "https://ab39-157-51-85-91.ngrok-free.app/predict_spe",
+    PREDICT_ASIGNEE_POINT_URL : "https://ab39-157-51-85-91.ngrok-free.app/predict_assignee"
+}
 
-const trainMockData = [
-    {
-      "ISSUE TYPE": "Task",
-      "STORY POINT ESTIMATE": 3,
-      "STORY POINT": null,
-      "SUMMARY": "Update existing servlet implementation extend BaseControllerServlet MyCase, ODR & XChange applications add authentication filters",
-      "DESCRIPTION": "undefinedundefined",
-      "ASSIGNEE": "VISHNU"
-    },
-    {
-      "ISSUE TYPE": "Task",
-      "STORY POINT ESTIMATE": 5,
-      "STORY POINT": null,
-      "SUMMARY": "Research: Add ability to set Interview Condition logic for Templates",
-      "DESCRIPTION": "Currently, conditional logic such as Interview Conditions is set on a field level per Content Form, and does not retain across Template usage in multiple Content Forms. Need to add the ability to set Interview Condition logic within a Template, such that admins can define Interview Conditions once within a Template, and reuse the same conditional logic by including the same Template within multiple Content Forms.",
-      "ASSIGNEE": "VARSHINEE VENKATESAN"
-    },
-    {
-      "ISSUE TYPE": "Task",
-      "STORY POINT ESTIMATE": 5,
-      "STORY POINT": null,
-      "SUMMARY": "Continuation - Form.io documentation",
-      "DESCRIPTION": "prepare documentation for form.io styling and configuration Documentation link: FormIO Styling Document.docx",
-      "ASSIGNEE": "RAJALAKSHMI"
-    },
-    {
-      "ISSUE TYPE": "Task",
-      "STORY POINT ESTIMATE": 5,
-      "STORY POINT": null,
-      "SUMMARY": "Continuation - Research: integrate Rasa with Twilio Flex",
-      "DESCRIPTION": "Need to explore integrating Rasa with Twilio Flex, as an alternative to using Google Dialogflow.",
-      "ASSIGNEE": "VARSHINEE VENKATESAN"
-    },
-    {
-      "ISSUE TYPE": "Task",
-      "STORY POINT ESTIMATE": 3,
-      "STORY POINT": null,
-      "SUMMARY": "Continuation - Add Gitlab pipeline to generate CourtsCommon Jar file to push to Nexus",
-      "DESCRIPTION": "Add Gitlab pipeline to generate CourtsCommon Jar file to push to NexusundefinedRepo URL: https://slc-git01.utahcourts.local/development/all_dev/commonapps/courtscommon.git",
-      "ASSIGNEE": "KEERTANA"
-    },
-    {
-      "ISSUE TYPE": "Task",
-      "STORY POINT ESTIMATE": 5,
-      "STORY POINT": null,
-      "SUMMARY": "Continuation - Add FormEngine to GitLab DevOps Operation",
-      "DESCRIPTION": "undefinedRepo URL: https://slc-git01.utahcourts.local/development/orange_dev/interview-apps/formengineWAS: https://dev-slc-ws-node03.utahcourts.local:9043/ibm/consoleApplication Name: FormEngineV5Context path: /FormEngineV5 App URL: https://devapps.utcourts.gov/FormEngineV5Servers to run :WebSphere:cell=dev-slc-ws-cell01,node=dev-slc-ws-ihs02,server=dev-slc-ws-ihs02\nWebSphere:cell=dev-slc-ws-cell01,node=dev-slc-ws-ihs01,server=dev-slc-ws-ihs01\nWebSphere:cell=dev-slc-ws-cell01,cluster=Application_Cluster",
-      "ASSIGNEE": "KEERTANA"
-    },
-    {
-      "ISSUE TYPE": "Task",
-      "STORY POINT ESTIMATE": 3,
-      "STORY POINT": null,
-      "SUMMARY": "Update existing servlet implementation extend BaseControllerServlet MyCase, ODR & XChange applications add authentication filters",
-      "DESCRIPTION": "undefinedundefined",
-      "ASSIGNEE": "VISHNU"
-    }
-]
 
 const App = () => {
 
-    // const [isAllChecked, setAllChecked] = useState(false);
-    // const [selectedRows, setSelectedRows] = useState([]);
-    const [sprints, setSprints] = useState([]);
-    const context = useProductContext();
+    /**
+     * @author Keerthana Ravindran 
+     * @description From the list of available sprints on the Jira board, get the current active sprint
+     */
     const [currentSprint, setCurrentSprint] = useState('');
-    let currentProjectKey = context.platformContext.projectKey;
-
-    const fetchEvents = async () => {
-        const context = useProductContext();
-        const res = await api
-            .asApp()
-            .requestJira(route`/rest/api/3/search?jql=project=${currentProjectKey} AND sprint in openSprints()`);
-            const data = await res.json();
-        
-            return data;
-    };
-
     useEffect(async()=>{
         const response = await api.asApp()
         .requestJira(route`/rest/agile/1.0/board/1/sprint`);
@@ -95,83 +34,70 @@ const App = () => {
         setCurrentSprint(currentSpr.name);
     },[])
 
+    /**
+     * @author Naveen kumar N
+     * @returns Header elements of the table
+     */
     const renderTableHeaders = () => {
         return <Fragment>
             <Head>
-                {/* <Cell><CheckboxGroup name="products"><Checkbox label="select all" isChecked={isAllChecked} onChange={handleCheckAll} /></CheckboxGroup></Cell> */}
-                <Cell><Text>Story Name</Text></Cell>
-                <Cell><Text>Description/Summary</Text></Cell>
-                <Cell><Text>Estimated Story Points</Text></Cell>
-                <Cell><Text>Suggested Developer</Text></Cell>
+                <Cell><Text>{appConstants.TABLE_HEADER_CONSTANTS.STORY_NAME}</Text></Cell>
+                <Cell><Text>{appConstants.TABLE_HEADER_CONSTANTS.DESCRIPTION}</Text></Cell>
+                <Cell><Text>{appConstants.TABLE_HEADER_CONSTANTS.STORY_POINTS}</Text></Cell>
+                <Cell><Text>{appConstants.TABLE_HEADER_CONSTANTS.SUGGESTED_DEVELOPER}</Text></Cell>
                 </Head>
         </Fragment>
     }
-
-    // const handleCheckRow = (id) => {
-    //     const index = selectedRows.indexOf(id);
-    //     if (index !== -1) {
-    //       setSelectedRows(selectedRows.filter(row => row !== id));
-    //     } else {
-    //       setSelectedRows([...selectedRows, id]);
-    //     }
-    // };
-
-    // const renderCheckBox = (id) =>{
-    //     return <Checkbox isChecked={selectedRows.includes(id)} onChange={()=>handleCheckRow(id)} />
-    // }
-
+    /**
+     * @author Naveen kumar N
+     * @param  issueType fetched fron the story
+     * @returns task column of the table
+     */
     const renderTask= (issueType) =>{
         return <Cell><Text><StatusLozenge text={issueType} appearance="inprogress" /></Text></Cell>
     }
 
+    /**
+     * @author Naveen kumar N
+     * @param descriptions of the story fetched
+     * @returns description column of the table
+     */
     const renderDescription = (descriptions) => {
         return <Cell>{descriptions.map(function (desc, i) {
             return <Text> {desc} </Text>
         })}</Cell>
     }
 
+    /**
+     * @author Naveen kumar N
+     * @param point story points that need to be populated
+     * @returns story points column of the table
+     */
     const renderPoints = (point) => {
         return <Cell><Text>{point}</Text></Cell>
     }
 
-    const renderDevHours = () => {
-        return <Cell><Text>8</Text></Cell>
-    }
-
+    /**
+     * @author Naveen kumar N
+     * @param developer Developer name that need to be populated
+     * @returns Developer column of the table
+     */
     const renderSuggestedDeveloper = (developer) => {
         return <Cell><Text>{developer}</Text></Cell>
     }
 
-    const renderSprintDropdown = () => {
-        const options = [
-          { label: 'Sprint 1', value: 'Sprint 1' },
-          { label: 'Sprint 2', value: 'Sprint 2' },
-          { label: 'Sprint 3', value: 'Sprint 3' }
-        ];
-
-        const handleOptionChange = (event) => {
-            const selectedOption = event.target.value;
-            // Do something with the selected option
-            setSelectedSprint(selectedOption);
-        }
-
-        return (
-            <Select name="select-sprint" label="Select Sprint" onSelectChange={handleOptionChange}>
-              {options.map(option => (
-                <Option value={option.value} label={option.label}/>
-              ))}
-            </Select>
-        );
-    }
-    const [storyPoints, setStoryPoints] = useState([]);
-    const renderStoryPointButton = () =>{
-        
+    /**
+     * @author Keerthana Ravindran
+     * @description For all the Issues related to the current sprint, estimate the Story points
+     * @returns Displays an action button called "Estimate Story Point"
+     */
+    const renderStoryPointButton = () =>{        
         let reqbody = {
             "model": "lr.sav",
             "data": issueData
         };
         const handleStoryPoint = async() => {
-            const res = await fetch("https://ab39-157-51-85-91.ngrok-free.app/predict_spe",
+            const res = await fetch(appConstants.PREDICT_STORY_POINT_URL,
             {
                 method : "POST",
                 headers : {
@@ -179,63 +105,33 @@ const App = () => {
                 },
                 body : JSON.stringify(reqbody)
             }).then(response => response.text())
-                    .then(data => {
-                        let storyPointArrData = JSON.parse(data)
-                        let story_points = storyPointArrData["DATA_PREDICTION"]
-                        
-                        issueArr.forEach((issue, i)=>{
-                            issue.points = story_points[i];
-                        })
-
-                        setIssueArr([...issueArr])
-
-                        setStoryPoints([...story_points]
-                            )})
-            console.log('story point button clicked!');
-            
+                .then(data => {
+                    let storyPointArrData = JSON.parse(data)
+                    let story_points = storyPointArrData["DATA_PREDICTION"]
+                    issueArr.forEach((issue, i)=>{
+                        issue.points = story_points[i];
+                    })
+                    setIssueArr([...issueArr])
+                })
         };
         
         return (
-            // <Fragment>
-                <Button text="Estimate Story Point" onClick={handleStoryPoint} />
-            // </Fragment>
+            <Button text="Estimate Story Point" onClick={handleStoryPoint} />
         );
     }
 
-    const renderTrainButton = () =>{
-        const [train, setTrain] = useState('');
-        const handleTrainButton = async() => {
-                const res = await fetch("https://ab39-157-51-85-91.ngrok-free.app/train",{
-                method: "POST", 
-                mode: "cors",
-                cache: "no-cache", 
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(trainMockData),
-            }
-            ).then(response => response.text())
-                    .then(data => setTrain(data))
-            console.log('story point button clicked!');
-            
-        };
-        
-        return (
-            // <Fragment>
-                <Button text="Train" onClick={handleTrainButton} />
-                // {/* <Text>{JSON.stringify(train)}</Text> */}
-            // </Fragment>
-        );
-    }
-
-    // const [suggestedDeveloper, setSuggestedDeveloper] = useState([]);
+    /**
+     * @author Keerthana Ravindran
+     * @description For all the Issues related to the current sprint, predict the Developer to be assigned
+     * @returns Displays an action button called "Suggest Developer"
+     */
     const renderSuggestedDeveloperButton = () =>{
         let reqbody = {
             "model": "lr.sav",
             "data": issueData
         };
         const handleSuggestedDeveloper = async() => {
-            const res = await fetch("https://ab39-157-51-85-91.ngrok-free.app/predict_assignee",
+            const res = await fetch(appConstants.PREDICT_ASIGNEE_POINT_URL,
             {
                 method : "POST",
                 headers : {
@@ -243,20 +139,15 @@ const App = () => {
                 },
                 body : JSON.stringify(reqbody)
             }).then(response => response.text())
-                    .then(data => {
-                        let storyPointArrData = JSON.parse(data)
-                        let story_points = storyPointArrData["DATA_PREDICTION"]
-                        
-                        issueArr.forEach((issue, i)=>{
-                            issue.suggestedDeveloper = story_points[i];
-                        })
-
-                        setIssueArr([...issueArr])
-
-                        // setStoryPoints([...story_points])
-                        })
+                .then(data => {
+                    let storyPointArrData = JSON.parse(data)
+                    let story_points = storyPointArrData["DATA_PREDICTION"]
+                    issueArr.forEach((issue, i)=>{
+                        issue.suggestedDeveloper = story_points[i];
+                    })
+                    setIssueArr([...issueArr])
+                })
         };
-
         return (
             <Button text="Suggest Developers" onClick={handleSuggestedDeveloper} />
         );
@@ -265,6 +156,10 @@ const App = () => {
     const [issueArr,setIssueArr] = useState([]);
     const [issueData,setIssueData] = useState([]);
 
+    /**
+     * @author Keerthana Ravindran
+     * @description Fetches all issues related to the current active sprint and also populate object for performing other POST requests for prediction calls
+     */
     useEffect(async()=>{
         const res = await api
         .asApp()
@@ -272,106 +167,65 @@ const App = () => {
         const events = await res.json();
         let resArray = [];
         events.issues.forEach(issue => {
-
             var issueDescriptionMap = {}
-           
             if (issue.fields.issuetype.name == 'Task') {
-           
-           
-           
-           
-            var descArr = []
-           if(issue.fields.description.content!=null ){
-               issue.fields.description.content.forEach(contentElement => {
-           
-                   if(contentElement.content!=null){
-                       contentElement.content.forEach(content => {
-                  
-                           if (content.type = "Text") {
-                          
-                           descArr.push(content.text)
-                          
-                           }
-                          
-                       });
-                   }
-                   
-             
-               });
-           }
-            
-           
-            issueDescriptionMap.type = issue.fields.summary;
-           
-            issueDescriptionMap.description = descArr
-           
-            resArray.push(issueDescriptionMap)
-            
-           
-            }
-           
-            });
-
-            setIssueArr([...resArray])
-            let issueDataStruct = [];
-            events.issues.forEach(issue => {
-
-                let desc = '';
+                var descArr = []
                 if(issue.fields.description.content!=null ){
                     issue.fields.description.content.forEach(contentElement => {
-                
                         if(contentElement.content!=null){
                             contentElement.content.forEach(content => {
-                    
                                 if (content.type = "Text") {
-                            
-                                    desc+= content.text+'\n';
-                            
+                                    descArr.push(content.text)
                                 }
-                            
                             });
                         }
-                        
-                
                     });
                 }
-
-                let issueObj = {
-                    "ISSUE TYPE" : issue.fields.issuetype.name,
-                    "STORY POINT ESTIMATE" : issue.fields.customfield_10016,
-                    "STORY POINT" : null,
-                    "SUMMARY" : issue.fields.summary,
-                    "DESCRIPTION" : desc,
-                    "ASSIGNEE" : issue.fields.assignee
-                }
-
-                issueDataStruct.push(issueObj);
-                
-            })
-
-            setIssueData([...issueDataStruct]);
-           
+                issueDescriptionMap.type = issue.fields.summary;
+                issueDescriptionMap.description = descArr
+                resArray.push(issueDescriptionMap)
+            }   
+        });
+        setIssueArr([...resArray])
+        let issueDataStruct = [];
+        events.issues.forEach(issue => {
+            let desc = '';
+            if(issue.fields.description.content!=null ){
+                issue.fields.description.content.forEach(contentElement => {
+                    if(contentElement.content!=null){
+                        contentElement.content.forEach(content => {
+                            if (content.type = "Text") {
+                                desc+= content.text+'\n';
+                            }
+                        });
+                    }
+                });
+            }
+            let issueObj = {
+                "ISSUE TYPE" : issue.fields.issuetype.name,
+                "STORY POINT ESTIMATE" : issue.fields.customfield_10016,
+                "STORY POINT" : null,
+                "SUMMARY" : issue.fields.summary,
+                "DESCRIPTION" : desc,
+                "ASSIGNEE" : issue.fields.assignee
+            }
+            issueDataStruct.push(issueObj);
+        })
+        setIssueData([...issueDataStruct]);
     },[])
 
  return (
     <ProjectPage>
         <Heading size="Medium"><Text>{currentSprint}</Text></Heading>
-        <Heading size="Medium"></Heading> 
         <ButtonSet>
             {renderStoryPointButton()}
             {renderSuggestedDeveloperButton()}
         </ButtonSet>
-        <Heading size="Medium"></Heading>
         <Table>
             {renderTableHeaders()}
             {issueArr.map(function (issue, i) {
                 return <Fragment>
                         <Row>
-                            {/* <Cell>
-                                <CheckboxGroup name={"products"}>
-                                    {renderCheckBox(i)}
-                                </CheckboxGroup>    
-                            </Cell> */}
                             {renderTask(issue.type)}    
                             {renderDescription(issue.description)}
                             {renderPoints(issue.points)}
@@ -380,14 +234,10 @@ const App = () => {
                     </Fragment>
                 }) 
              }
-             
-         </Table>
-         {/* <Text>{JSON.stringify(events)}</Text> */}
+        </Table>
     </ProjectPage>
     );
 };
-
-
 
 
 export const run = render(<App />);
